f8b816c9fd387a256159cbf8adbd64eec4fb49e4
Diego Randolp <132240895+diegorandolp@users.noreply.github.com>
2025-04-01 01:02:11 -0500

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- NEOVIM BASIC CONFIG
+
+-- mkdir -p ~/.config/nvim
+-- nvim ~/.config/nvim/init.lua
+
+vim.o.number = true        -- Show line numbers
+vim.o.relativenumber = true -- Relative line numbers
+vim.o.clipboard = "unnamedplus" -- Use system clipboard
+vim.o.expandtab = true      -- Use spaces instead of tabs
+vim.o.shiftwidth = 4        -- Set indentation width
+vim.o.tabstop = 4           -- Number of spaces per tab
+vim.o.smartindent = true    -- Auto-indent new lines
+
+-- :source ~/.config/nvim/init.lua
