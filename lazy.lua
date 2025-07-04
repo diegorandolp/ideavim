c392cf685ffaac00d1b4d5035fc2e2acecc94367
Diego Randolp <132240895+diegorandolp@users.noreply.github.com>
2025-07-04 00:10:43 -0500

    Update lazy.lua

--- lazy.lua
+++ lazy.lua
@@ -1,45 +1,52 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- add LazyVim andi mport its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import your plugins
-    { import = "plugins" },
-  },
-  defaults = {
-      lazy = false, -- only LazyVim plugins will be lazy-loaded, others will load during startup
-      version = false, -- always use the latest git commit
-  },
-  cheker = {
-      enabled = true, -- automatically check plugin updates periodically
-      notify = false, -- notify on update
-  },
-  performance = {
-    rtp = { -- diable some plugins
-      disable_plugins = {
-        "gzip",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },   
-    },
-  },
+	spec = {
+		-- 1. Core LazyVim
+		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
+
+		-- 2. LazyVim “extras” (language support, UI, DAP, etc.)
+		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "lazyvim.plugins.extras.lang.json" },
+		--{ import = "lazyvim.plugins.extras.lang.rust" },
+		-- you can also enable ui extras, dap extras, etc:
+		--
+		-- 3. Your own plugins
+		{ import = "plugins" },
+	},
+	defaults = {
+		lazy = false, -- only LazyVim plugins will be lazy-loaded, others will load during startup
+		version = false, -- always use the latest git commit
+	},
+	cheker = {
+		enabled = true, -- automatically check plugin updates periodically
+		notify = false, -- notify on update
+	},
+	performance = {
+		rtp = { -- diable some plugins
+			disable_plugins = {
+				"gzip",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
